0b5809d094611283f9aa2fd96d9908a0dcae2d95
hal <pn_frt_hal>
2024-01-19 15:37:55 +0900

    절대경로 설정

--- minibim/src/App.js
+++ minibim/src/App.js
@@ -1,26 +1,13 @@
-import logo from './logo.svg';
-import style from "./assets/scss/reset.module.css";
-import './App.css';
 
-function App() {
+import style from "@style/reset.module.css";
+import Header from "@components/common/header";
+
+function App() {  
   return (
     <div className={style.app}>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> 
-      </header>
+      <Header />
     </div>
   );
-}
+} 
 
 export default App;
