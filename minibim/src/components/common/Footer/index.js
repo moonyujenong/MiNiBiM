f34f5f646e423a04a9cd986852db8a7e46f18432
hal <pn_frt_hal>
2024-02-07 12:14:31 +0900

    font 세팅 순서 변경

--- minibim/src/components/common/Footer/index.js
+++ minibim/src/components/common/Footer/index.js
@@ -9,7 +9,7 @@ function Footer() {
         <img alt="logo" src={logo} className={style.logo} />
         <ul className={style.foot_info}>
           <li className={style.info_item}>
-            (주)미니빔 <p>|</p> 대표 문유정 임지영
+            (주) 미니빔 <p>|</p> 대표 문유정 임지영
           </li>
           <li className={style.info_item}>
             사업자등록번호 : 203-2933-20392
