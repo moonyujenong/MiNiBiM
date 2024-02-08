d95e08d124d2a190643ca19398b5a68aca2b8d5b
hal <pn_frt_hal>
2024-02-08 09:28:37 +0900

    add main mission section

--- minibim/src/App.js
+++ minibim/src/App.js
@@ -1,12 +1,15 @@
 import style from '@style/reset.module.css';
 import Header from '@components/common/Header';
 import Footer from '@components/common/Footer';
+import Mission from './components/Mission';
 
 function App() {
   return (
     <div className={style.wrap}>
       <Header />
-      <div className={style.contents}></div>
+      <div className={style.contents}>
+        <Mission />
+      </div>
       <Footer />
     </div>
   );
