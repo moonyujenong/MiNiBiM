0f2ccb98ac92151e5f4116fb5b759cc6c79183a9
hal <pn_frt_hal>
2024-02-14 14:12:45 +0900

    hover & settimeout

--- minibim/src/components/Process/index.js
+++ minibim/src/components/Process/index.js
@@ -1,10 +1,18 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { processList } from './processList';
 import RadiusInText from '../common/RadiusInText';
 import style from './index.module.css';
 
 function Process() {
   const [isTimeActive, setIsTimeActive] = useState(0);
+
+  useEffect(() => {
+    const interval = setTimeout(() => {
+      setIsTimeActive((idx) => (idx + 1) % processList.length);
+    }, 3000);
+
+    return () => clearTimeout(interval);
+  }, [isTimeActive]);
 
   return (
     <div className={style.container}>
@@ -13,8 +21,8 @@ function Process() {
       <h3 className={style.title}>꼼꼼한 작업 과정으로 이루어집니다!</h3>
 
       <ul className={style.process_list}>
-        {processList.map((item) => (
-          <li className={style.process} key={item.id}>
+        {processList.map((item, idx) => (
+          <li className={`${style.process} ${idx === isTimeActive ? style.active : ''}`} key={item.id}>
             <img alt="process img" src={item.img} />
             <h3 className={style.process_title}>
               <p>{item.id + 1}</p> {item.title}
