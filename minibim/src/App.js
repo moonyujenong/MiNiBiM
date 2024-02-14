61b9ccd129a464e740560ed9f1dadd73cecc4b72
hal <pn_frt_hal>
2024-02-14 17:47:36 +0900

    contact 폼 mobile

--- minibim/src/App.js
+++ minibim/src/App.js
@@ -4,6 +4,7 @@ import Footer from '@components/common/Footer';
 import Mission from './components/Mission';
 import ClientReview from './components/ClientReview';
 import Process from './components/Process';
+import Contact from './components/Contact';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Mission />
         <ClientReview />
         <Process />
+        <Contact />
       </div>
       <Footer />
     </div>
