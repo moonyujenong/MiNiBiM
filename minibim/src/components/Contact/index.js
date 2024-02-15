4fcf0d3ce92ebc543a37817ad4f59955ec9b787a
hal <pn_frt_hal>
2024-02-15 10:55:40 +0900

    contact 폼 pc

--- minibim/src/components/Contact/index.js
+++ minibim/src/components/Contact/index.js
@@ -17,49 +17,53 @@ function Contact() {
 
   return (
     <div className={style.container}>
-      <h3 className={style.title}>Contact</h3>
+      <div className={style.wrap}>
+        <h3 className={style.title}>Contact</h3>
 
-      <form action="">
-        <div className={style.enter_list}>
-          {contactList.map((item) => (
-            <div className={style.form_box} key={item.id}>
-              <h3 className={`${style.category} ${item.requir === true ? style.requir : ''}`}>{item.name}</h3>
-              {item.id === 6 ? (
-                <textarea placeholder={item.placeHolder} className={style.user_text_box}></textarea>
-              ) : (
-                <>
-                  {item.type === 'file' ? (
-                    <label htmlFor="fileName">
-                      <input
-                        type="texet"
-                        readOnly
-                        placeholder={isSelectFile ? '' : item.placeHolder}
-                        value={isSelectFile && isSelectFile}
-                      />
-                      <input
-                        type="file"
-                        ref={fileRef}
-                        onChange={handleFileInput}
-                        id="fileName"
-                        style={{ display: 'none' }}
-                      />
-                      <button type="button" onClick={handleFileBtn} className={style.file_upload}>
-                        *파일선택
-                      </button>
-                    </label>
-                  ) : (
-                    <input type={item.type} placeholder={item.placeHolder} className={style.user_input} />
-                  )}
-                </>
-              )}
-            </div>
-          ))}
-        </div>
+        <form action="">
+          <div className={style.enter_list}>
+            {contactList.map((item) => (
+              <div className={style.form_box} key={item.id}>
+                <h3 className={`${style.category} ${item.requir === true ? style.requir : ''}`}>{item.name}</h3>
+                {item.id === 6 ? (
+                  <textarea placeholder={item.placeHolder} className={style.user_text_box}></textarea>
+                ) : (
+                  <>
+                    {item.type === 'file' ? (
+                      <label htmlFor="fileName">
+                        <input
+                          type="texet"
+                          readOnly
+                          placeholder={isSelectFile ? '' : item.placeHolder}
+                          value={isSelectFile && isSelectFile}
+                          onClick={handleFileBtn}
+                          className={style.file_name_input}
+                        />
+                        <input
+                          type="file"
+                          ref={fileRef}
+                          onChange={handleFileInput}
+                          id="fileName"
+                          style={{ display: 'none' }}
+                        />
+                        <button type="button" onClick={handleFileBtn} className={style.file_upload}>
+                          *파일선택
+                        </button>
+                      </label>
+                    ) : (
+                      <input type={item.type} placeholder={item.placeHolder} className={style.user_input} />
+                    )}
+                  </>
+                )}
+              </div>
+            ))}
+          </div>
 
-        <button className={style.submit_btn} type="submit">
-          문의 보내기
-        </button>
-      </form>
+          <button className={style.submit_btn} type="submit">
+            문의 보내기
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
