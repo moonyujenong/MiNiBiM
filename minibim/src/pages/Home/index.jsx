f2b98cee8ea3811460618ed3be11d6e9ade76c63
moon <moon@moonui-iMac.local>
2024-08-10 14:30:50 +0900

    main-works UI

---
+++ minibim/src/pages/Home/index.jsx
@@ -0,0 +1,22 @@
+import style from "./index.module.css";
+import Layouts from "../../common/components/Layouts";
+import Works from "./Works";
+
+function Home () {
+  return (
+    <Layouts>
+      <div className={style.container}>
+        <h3 className={style.left_title}>
+        Crafting<br />New Possibilities<br />Creativity
+        </h3>
+        <h3 className={style.right_title}>
+        크레페 스튜디오는 평범한 아이디어를<br />현실로 실현 가능하게 해주며<br />창작의 한계를 허물어<br /> 함께 새로운 가능성을 열어 갑니다.
+        </h3>
+        
+        <Works />
+      </div>
+    </Layouts>
+  )
+}
+
+export default Home;
