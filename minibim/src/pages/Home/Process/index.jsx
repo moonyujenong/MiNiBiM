e9b344e5ca141ca3ed3199a44390abcc9f80d7f1
myj <sogml308@gmail.com>
2024-08-16 17:08:30 +0900

    process pc

--- minibim/src/pages/Home/Process/index.jsx
+++ minibim/src/pages/Home/Process/index.jsx
@@ -1,9 +1,14 @@
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import 'swiper/css/thumbs';
 import style from './index.module.css';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import useResponsive from '../../../common/hook/Responsive';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { process } from './process';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { FreeMode, Thumbs, Autoplay } from 'swiper/modules';
 
 function Process() {
   const screenSize = useResponsive();
@@ -25,16 +30,10 @@ function Process() {
 export default Process;
 
 export function MobileProcess() {
-  const screenSize = useResponsive();
-
-  useEffect(() => {
-    AOS.init();
-  }, [screenSize]);
-
   return (
     <div className={style.process_list_wrap}>
       {process.map((item) => (
-        <div className={style.list_item} key={item.id} {...(screenSize < 1280 ? { 'data-aos': 'fade-up' } : {})}>
+        <div className={style.list_item} key={item.id} data-aos="fade-up">
           <img alt="process img" src={item.img} />
           <div className={style.contents}>
             <h3 className={style.item_title}>
@@ -50,5 +49,57 @@ export function MobileProcess() {
 }
 
 export function PcProcess() {
-  return <div className={style.process_list}></div>;
+  const [thumbsSwiper, setThumbsSwiper] = useState(null);
+
+  return (
+    <div className={style.process_list}>
+      <Swiper
+        thumbs={{ swiper: thumbsSwiper && !thumbsSwiper.destroyed ? thumbsSwiper : null }}
+        modules={[FreeMode, Thumbs, Autoplay]}
+        slidesPerView={'auto'}
+        className={style.thumb_swipe}
+        allowTouchMove={false}
+        loop={true}
+        spaceBetween={10}
+        autoplay={{
+          delay: 5000,
+          disableOnInteraction: false,
+        }}
+      >
+        {process.map((item) => (
+          <SwiperSlide key={item.id} className={style.img_box}>
+            <img alt="process img" src={item.img} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+
+      <Swiper
+        onSwiper={setThumbsSwiper}
+        slidesPerView={'auto'}
+        freeMode={true}
+        watchSlidesProgress={true}
+        modules={[FreeMode, Thumbs, Autoplay]}
+        className={style.text_swipe}
+        allowTouchMove={false}
+        touchRatio={false}
+        loop={true}
+        autoplay={{
+          delay: 5000,
+          disableOnInteraction: false,
+        }}
+      >
+        {process.map((item) => (
+          <SwiperSlide key={item.id} className={style.text_box}>
+            <div className={style.left_title}>
+              <p>{item.num}</p>
+              <p>{item.title}</p>
+            </div>
+            <div className={style.right_title}>
+              <p>{item.desc}</p>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
 }
