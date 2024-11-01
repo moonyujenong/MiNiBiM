29dd271bb1af7861fe8549ef468cc78cbc083d11
myj <sogml308@gmail.com>
2024-11-01 18:05:00 +0900

    데이터 연동

--- minibim/src/pages/Works/index.jsx
+++ minibim/src/pages/Works/index.jsx
@@ -2,28 +2,39 @@ import { useEffect, useState } from "react";
 import style from "./index.module.css";
 import { database } from "../../firebaseConfig";
 import Layouts from '../../common/components/Layouts/index';
-import { doc, getDoc } from "firebase/firestore/lite";
+import { doc, getDoc, getDocs , collection, query, orderBy, limit} from "firebase/firestore/lite";
 
 function Works () {
-    const [postData, setPostData] = useState(null);
+    const [postData, setPostData] = useState([]);
 
     async function getDocuments(posts) {
-        const list = doc(database, "crepeWorks", posts);
-        console.log(1111, list)
-        const listSnapshot = await getDoc(list);
 
-        if (!listSnapshot.exists()) {
-            return console.log("문서가 존재하지 않습니다.") 
-        } 
+        const q = query(collection(database, "crepeWorks"));
+        const querySnapshot = await getDocs(q);
 
-        setPostData(listSnapshot.data());
+        let temp = [];
+
+        querySnapshot.forEach((doc) => {
+            temp.push(doc.data())
+            });
+        return temp;
     }
+
+    useEffect(() => {
+        getDocuments().then(data => {
+            setPostData([...data]);
+            console.log(data)
+        });
+    }, []);
 
     return (
         <Layouts>
             <div className={style.container}>
                 <h3 className={style.title}>Board List</h3>
                 <ul>
+                    {postData.map(post => {
+                        return <div>{post.title} - {post.category}</div>
+                    })}
                 </ul>
             </div>
         </Layouts>
